17c736e933699128d36b1df16eaf11d5c33ffc36
martinacev <103221910+martinacev@users.noreply.github.com>
2023-12-13 11:12:28 +0100

    display-movies

--- src/components/ListMovies/ListMovies.jsx
+++ src/components/ListMovies/ListMovies.jsx
@@ -1,22 +1,33 @@
-import CardMovie from "../CardMovie/CardMovie";
-import classes from "./listMovies.module.css";
+import  { useState, useEffect } from 'react';
+import CardMovie from '../CardMovie/CardMovie';
+import TheMovie from '../TheMovie/TheMovie';
+import classes from './listMovies.module.css';
 
 export default function ListMovies({ movies }) {
-	return (
-		<div className={classes.wrapper}>
-		<ul className={classes.container}>
-			{movies.map((movie) => {
-				return (
-					<li key={movie.id} className={classes["movie-item"]}>
-						<CardMovie
-							title={movie.title}
-							description={movie.overview}
-							image={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-						/>
-					</li>
-				);
-			})}
-		</ul>
-		</div>
-	);
+  const [selectedMovie, setSelectedMovie] = useState();
+
+  useEffect(() => {
+
+  }, [selectedMovie]);
+
+  const handleCardClick = (movie) => {
+    setSelectedMovie(movie);
+  };
+
+  return (
+    <div className={classes.wrapper}>
+      <ul className={classes.container}>
+        {movies.map((movie) => (
+          <li key={movie.id} className={classes['movie-item']} onClick={() => handleCardClick(movie)}>
+            <CardMovie
+              title={movie.title}
+              description={movie.overview}
+              image={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+            />
+          </li>
+        ))}
+      </ul>
+      <TheMovie selectedMovie={selectedMovie} />
+    </div>
+  );
 }
--- src/components/TheMovie/TheMovie.jsx
+++ src/components/TheMovie/TheMovie.jsx
@@ -1,11 +1,25 @@
 import classes from '../TheMovie/theMovie.module.css';
+import favorite from '../../../public/favorite.png';
 
-const TheMovie = () => {
-    return (
-        <div className={classes.container}>
-            <h2 className={classes.colorPrivremeno}>Selected movie</h2>
+const TheMovie = ({ selectedMovie }) => {
+  return (
+    <div>
+      {selectedMovie && (
+        <div className={classes.wrapper}>
+            <img
+              src={`https://image.tmdb.org/t/p/original/${selectedMovie.poster_path}`}
+              alt={selectedMovie.title}
+              className={classes.movieImage}
+            />
+          <h3>{selectedMovie.title}</h3>
+          <p>{selectedMovie.overview}</p>
+          <div className={classes.favorite}>
+            <img src={favorite} alt="favorite" />
+          </div>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
 export default TheMovie;
