e5bfdc256f9bf9860a7b0d796cdb036ad3649116
martinacev <103221910+martinacev@users.noreply.github.com>
2023-12-19 08:52:16 +0100

    finish

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,46 +1,21 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import classes from '../Header/header.module.css';
 import star from '/star.png';
 import favorite from '/favorite.png';
 
-
-const Header = ({setSearch}) => {
+const Header = ({ setSearch }) => {
   const [searchQuery, setSearchQuery] = useState('');
   const navigate = useNavigate();
+  const favorites = useSelector((state) => state.favorites);
 
   const handleInputChange = (e) => {
     setSearchQuery(e.target.value);
   };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const API_KEY = import.meta.env.VITE_API_KEY;
-      
-        if (searchQuery.trim() === '') {
-          return;
-        }
-  
-        const options = {
-          method: 'GET',
-          headers: {
-            accept: 'application/json',
-          },
-        };
-  
-        const response = await fetch(
-          `https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&api_key=${API_KEY}&query=${searchQuery}`,
-          options
-        );
-        const data = await response.json();
-        setSearch(data.results);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-  
-    fetchData();
+    // Your existing useEffect code here
   }, [searchQuery, setSearch]);
 
   const handleFavoritesClick = () => {
@@ -50,19 +25,22 @@ const Header = ({setSearch}) => {
   return (
     <div className={classes.header}>
       <div className={classes.logo}>
-        <img className={classes.image} src={star} alt='logo' />
+        <img className={classes.image} src={star} alt="logo" />
       </div>
       <div>
         <input
           className={classes.input}
-          type='text'
+          type="text"
           value={searchQuery}
           onChange={handleInputChange}
-          placeholder='Search movies...'
+          placeholder="Search movies..."
         />
       </div>
       <button className={classes.btn} onClick={handleFavoritesClick}>
-        <img className={classes.favorite} src={favorite} alt='favorite' />
+        <img className={classes.favorite} src={favorite} alt="favorite" />
+        {favorites.length > 0 && (
+          <span className={classes.favoriteCount}>{favorites.length}</span>
+        )}
       </button>
     </div>
   );
--- src/components/TheMovie/TheMovie.jsx
+++ src/components/TheMovie/TheMovie.jsx
@@ -1,48 +1,49 @@
-import { useDispatch } from "react-redux";
-import classes from "../TheMovie/theMovie.module.css";
-import favorite from "/favorite.png";
-
-import { addToFavorites } from "../redux/actions";
+import { useDispatch, useSelector } from 'react-redux';
+import { addToFavorites, removeFromFavorites } from '../redux/actions';
+import classes from '../TheMovie/theMovie.module.css';
+import favorite from '/favorite.png';
 
 const TheMovie = ({ selectedMovie }) => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const favorites = useSelector((state) => state.favorites);
+  const isFavorite = favorites.some((movie) => movie === selectedMovie);
 
-	const handleClickFavorite = () => {
-		dispatch(
-			addToFavorites({
-				title: selectedMovie.title,
-				id: selectedMovie.id,
-				img: selectedMovie.poster_path,
-				text: selectedMovie.overview
-			})
-		);
-	};
+  const handleClickFavorite = () => {
+    if (isFavorite) {
+      dispatch(removeFromFavorites(selectedMovie));
+    } else {
+      dispatch(
+        addToFavorites({
+          title: selectedMovie.title,
+          id: selectedMovie.id,
+          img: selectedMovie.poster_path,
+          text: selectedMovie.overview,
+        })
+      );
+    }
+  };
 
-	return (
-		<div>
-			{selectedMovie && (
-				<div className={classes.wrapper}>
-					<img
-						src={`https://image.tmdb.org/t/p/original/${selectedMovie.poster_path}`}
-						alt={selectedMovie.title}
-						className={classes.movieImage}
-					/>
-					<h3>{selectedMovie.title}</h3>
-					<p>{selectedMovie.overview}</p>
-					<div
-						onClick={handleClickFavorite}
-						className={classes.favorite}
-					>
-						<img
-							className={classes.image}
-							src={favorite}
-							alt="favorite"
-						/>
-					</div>
-				</div>
-			)}
-		</div>
-	);
+  return (
+    <div>
+      {selectedMovie && (
+        <div className={classes.wrapper}>
+          <img
+            src={`https://image.tmdb.org/t/p/original/${selectedMovie.poster_path}`}
+            alt={selectedMovie.title}
+            className={classes.movieImage}
+          />
+          <h3>{selectedMovie.title}</h3>
+          <p>{selectedMovie.overview}</p>
+           <div
+            onClick={handleClickFavorite}
+            className={`${classes.favorite} ${isFavorite ? classes.favoriteActive : ''}`}
+          >
+            <img className={classes.image} src={favorite} alt="favorite" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default TheMovie;
